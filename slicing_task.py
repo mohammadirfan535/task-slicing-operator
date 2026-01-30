85a68d3993fa2cb232d9380659186ef6333dff10
mohammadirfan535 <mohammadksirfan@gmail.com>
2026-01-30 17:09:56 +0530

    task_file

---
+++ slicing_task.py
@@ -0,0 +1,13 @@
+text="pythonprogramming"
+print(text[0:6:1])
+print(text[6:13:1])
+print(text[11:17:1])
+print(text[0:17:2])
+print(text[-1:-16:-2])
+
+
+
+numbers=[10,20,30,40,50,60,70]
+print(numbers[0:3:1])
+print(numbers[4:7:1])
+print(numbers[2:6:1])
